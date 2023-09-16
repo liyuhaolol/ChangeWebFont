d55d1223e33b6718bee1f6be00b48993e8e89357
liyuhao <liyuhaoid@sina.com>
2023-09-16 20:56:00 +0800

    1.1

--- font.js
+++ font.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         修改网页字体
 // @namespace    https://github.com/liyuhaolol/ChangeWebFont
-// @version      1.0
+// @version      1.1
 // @description  覆盖网页的body默认字体样式
 // @author       菜狗子
 // @updateURL    https://fastly.jsdelivr.net/gh/liyuhaolol/ChangeWebFont/font.js
@@ -20,3 +20,56 @@ GM_addStyle(`
         font-family: 'MiSans', 'PingFang SC', 'Microsoft Yahei', Arial, sans-serif;
     }
 `);
+
+window.onload = function () {
+  changeDTelementWidth();
+  changeJpFont();
+};
+
+function changeDTelementWidth() {
+  var divElements = document.getElementsByClassName('detail_t detail_t1');
+  if (divElements != null) {
+    var divArray = Array.from(divElements);
+    divArray.forEach(function (divElement) {
+      var dlElement = divElement.querySelector('dl');
+      if (dlElement != null) {
+        var dtElements = dlElement.querySelectorAll('dt');
+        if (dtElements != null) {
+          var dtArray = Array.from(dtElements);
+          dtArray.forEach(function (dtElement) {
+            //console.log(dtElement);
+            dtElement.style.width = '5em';
+          });
+        }
+      }
+    });
+  }
+}
+
+function changeJpFont(){
+  //修改MB简介字体
+  var pElementTitle = document.getElementsByClassName('intro_txt');
+  if(pElementTitle != null){
+    //console.log(pElement);
+    pElementTitle[0].style.fontFamily = '宋体,SimSun,Arial,sans-serif';
+  }
+  //修改跳转按钮字体
+  var pElementJumpBtns= document.getElementsByClassName('linkbtn_s');
+  if(pElementJumpBtns != null){
+    var pArray = Array.from(pElementJumpBtns);
+    pArray.forEach(function (pElement) {
+      var aElement = pElement.querySelector('a');
+      if(aElement != null){
+        aElement.style.fontFamily = '宋体,SimSun,Arial,sans-serif';
+      }
+    });
+  }
+  //修改跳转按钮2字体
+  var pElementJumpBtn= document.getElementsByClassName('linkbtn_m');
+  if(pElementJumpBtn != null){
+    var aElement = pElementJumpBtn[0].querySelector('a');
+    if(aElement != null){
+      aElement.style.fontFamily = '宋体,SimSun,Arial,sans-serif';
+    }
+  }
+}
